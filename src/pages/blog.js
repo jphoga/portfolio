4424fcc06d6213a5da83a38cdd98bc46bc688cdb
Jan Hoga <janpaulhoga@gmail.com>
2019-09-02 17:44:50 +0900

    first commit - Gatsby

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
-class HomeIndex extends React.Component {
+class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
@@ -47,7 +47,7 @@ class HomeIndex extends React.Component {
   }
 }
 
-export default HomeIndex
+export default BlogIndex
 
 export const pageQuery = graphql`
   query {
