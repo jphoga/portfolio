2edadd9383c54a350ff1269c4be945d0b8527586
Jan Hoga <janpaulhoga@gmail.com>
2019-11-17 23:15:16 +0900

    next try: smaller picture for landing page

--- src/components/index-introduction.js
+++ src/components/index-introduction.js
@@ -9,7 +9,7 @@ const IndexPart = ({ className }) => (
   <StaticQuery
     query={graphql`
       query {
-        desktop: file(relativePath: { eq: "tokyo-air.png" }) {
+        desktop: file(relativePath: { eq: "tokyo-air.jpg" }) {
           childImageSharp {
             fluid(quality: 90, maxWidth: 1920) {
               ...GatsbyImageSharpFluid_withWebp
